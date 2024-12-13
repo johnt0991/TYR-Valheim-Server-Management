c46eef07368af9a226fd133f6dd89a6111e1aa68
johnt0991 <60276226+johnt0991@users.noreply.github.com>
2024-12-13 11:24:55 -0800

    working on bugs

--- valheim_code_bot.py
+++ valheim_code_bot.py
@@ -1,3 +1,4 @@
+# %% Modules
 import subprocess
 import time
 import ctypes
@@ -10,44 +11,37 @@ import requests
 import psutil
 from datetime import datetime, timedelta
 import re
+# %%
+# %% Global Variables
 
-
-# Path to your .bat script
 BAT_SCRIPT_PATH = "start_headless_server.bat"
-
-# Discord webhook URL
 WEBHOOK_URL = ""
-
-# Variables to hold server information
 session_name = ""
 server_ip = ""
 join_code = ""
 server_running = False
 process = None  # To hold the reference to the running process
-
-# Active processes list
 active_processes = []
-
-# Reset scheduling variables
 reset_enabled = False
 reset_interval = 6  # Default reset interval in hours
 reset_start_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).time()
 next_reset_time = None
-
 program_running = True
 window = None
+# %%
+# %% Message Posting Functions
 
-
-def send_to_console_tab(message):
+def send_to_action_tab(message):
     """Send a message to the console output text box."""
-    if text_area_console:  #if widget exists
-        text_area_console.insert(tk.END, message + "\n") #insert message and then new line
-        text_area_console.yview(tk.END)
+    if text_area_action:  #if widget exists
+        time_stamp = str(datetime.now("%H:%M:%S"))
+        text_area_action.insert(tk.END, message + " " + time_stamp + "\n") #insert message and then new line
+        text_area_action.yview(tk.END)
 
 def send_to_process_tab(message):
     """Sends an opened process to the processes tab"""
     if text_area_processes: #if widget exists
-        time_stamp = str(datetime.now())
+        time_stamp = str(datetime.now("%H:%M:%S"))
         text_area_processes.insert(tk.END, message + " " + time_stamp + "\n") #insert new message and then new line
         text_area_processes.yview(tk.END)
 
@@ -60,29 +54,48 @@ def send_to_discord(message):
     response = requests.post(WEBHOOK_URL, json=payload)
     if response.status_code != 204:
         print(f"Failed to send message: {response.status_code}, {response.text}")
-    else: send_to_console_tab("Message Successfully Posted to Discord.")
+    else: send_to_action_tab("Message Successfully Posted to Discord.")
+    
+def update_server_info(line):
+    """Update the Main tab with server information."""
+    server_info = f"Session: {session_name}\nServer IP: {server_ip}\nJoin Code: {join_code}\n"
+    text_area_main.delete(1.0, tk.END)
+    text_area_main.insert(tk.END, server_info)
+
+def update_text_widget(message, filter_type):
+    """Update the corresponding text widget based on filter type."""
+    # Always add the message to the All tab
+    text_area_all.insert(tk.END, message + '\n')
+    text_area_all.yview(tk.END)
+    
+    # Add the message to the specific tab if applicable
+    widgets = {
+        "warning": text_area_warning,
+        "error": text_area_error,
+        "join_code": text_area_join_code
+    }
+    if filter_type in widgets:
+        widget = widgets[filter_type]
+        widget.insert(tk.END, message + '\n')
+        widget.yview(tk.END)
+# %%
         
+# %% Server Run Functions
 def start_batch_script():
     """Runs the .bat script and captures the output."""
     global process, server_running, session_name, join_code, server_ip
     
     # Start the .bat script
-    process = subprocess.Popen(
-        BAT_SCRIPT_PATH,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        shell=True
-    )
+    process = subprocess.Popen(BAT_SCRIPT_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, shell=True)
     server_running = True
-    send_to_process_tab("Subprocess for valheim_server.exe started.")
+    send_to_process_tab(process)
     
     # Add process to active processes
     active_processes.append(process)
     update_active_processes()
 
     if reset_enabled and next_reset_time:
-        send_to_console_tab(f"The server has started. The next scheduled reset is at: {next_reset_time}")
+        send_to_action_tab(f"The server has started. The next scheduled reset is at: {next_reset_time}")
 
     # Monitor the server output line-by-line
     for line in process.stdout:
@@ -109,13 +122,43 @@ def start_batch_script():
         if "This is the serverIP used to register the server" in line:
             server_ip = line.split(': ')[-1]  # Extract the server IP
             update_server_info(line.strip())
+            
+def stop_server():
+    """Stop the server by sending CTRL+C (CTRL_C_EVENT) to the process."""
+    global process, server_running
+    try:
+        # Get the process ID of the valheim_server.exe process
+        for proc in psutil.process_iter(attrs=['pid', 'name']):
+            if "valheim_server.exe" in proc.info['name']:
+                pid = proc.info['pid']
+                break
+        
+        # Use ctypes to send CTRL+C (CTRL_C_EVENT) to the process
+        if pid:
+            ctypes.windll.kernel32.GenerateConsoleCtrlEvent(0, pid)  # 0 is for CTRL_C_EVENT
+            send_to_action_tab(f"Sent CTRL+C (CTRL_C_EVENT) to process {pid}.") #notating in console
+            # Optionally, wait and send 'y' (simulating confirmation)
+            time.sleep(5)
+            pyautogui.write('y')
+            pyautogui.press('enter')
+            server_running = False
+            send_to_action_tab("Server has successfully stopped.")
+            # Remove process from active processes once it is stopped
+            active_processes.remove(process)
+            update_active_processes()
 
-    server_running = False
-    update_server_info("Server has stopped.")
+            
+        else:
+            send_to_action_tab("Process not found.")
+    except Exception as e:
+        print(f"Error stopping the server: {e}")
     
-    # Remove process from active processes once it is stopped
-    active_processes.remove(process)
+    # Stop the process explicitly if needed
+    if process:
+        process.terminate()
+        process.wait()  # Ensure the process is terminated
     update_active_processes()
+# %%
 
 def update_active_processes():
     """Update the Active Processes tab."""
@@ -128,61 +171,6 @@ def update_active_processes():
     # Schedule the update to happen in the main thread
     window.after(0, update_gui)
 
-
-def update_server_info(line):
-    """Update the Main tab with server information."""
-    server_info = f"Session: {session_name}\nServer IP: {server_ip}\nJoin Code: {join_code}\n"
-    text_area_main.delete(1.0, tk.END)
-    text_area_main.insert(tk.END, server_info)
-
-def update_text_widget(message, filter_type):
-    """Update the corresponding text widget based on filter type."""
-    # Always add the message to the All tab
-    text_area_all.insert(tk.END, message + '\n')
-    text_area_all.yview(tk.END)
-    
-    # Add the message to the specific tab if applicable
-    widgets = {
-        "warning": text_area_warning,
-        "error": text_area_error,
-        "join_code": text_area_join_code
-    }
-    if filter_type in widgets:
-        widget = widgets[filter_type]
-        widget.insert(tk.END, message + '\n')
-        widget.yview(tk.END)
-
-def stop_server():
-    """Stop the server by sending CTRL+C (CTRL_C_EVENT) to the process."""
-    try:
-        # Get the process ID of the valheim_server.exe process
-        for proc in psutil.process_iter(attrs=['pid', 'name']):
-            if "valheim_server.exe" in proc.info['name']:
-                pid = proc.info['pid']
-                break
-        
-        # Use ctypes to send CTRL+C (CTRL_C_EVENT) to the process
-        if pid:
-            ctypes.windll.kernel32.GenerateConsoleCtrlEvent(0, pid)  # 0 is CTRL_C_EVENT
-
-            # Optionally, wait and send 'y' (simulating confirmation)
-            time.sleep(5)
-            pyautogui.write('y')
-            pyautogui.press('enter')
-
-            print(f"Sent CTRL+C (CTRL_C_EVENT) to process {pid}.")
-        else:
-            print("Process not found.")
-    except Exception as e:
-        print(f"Error stopping the server: {e}")
-    
-    # Stop the process explicitly if needed
-    global process
-    if process:
-        process.terminate()
-        process.wait()  # Ensure the process is terminated
-    update_active_processes()
-
 def enable_reset(reset_checkbox, reset_interval_entry, reset_start_time_dropdown):
     """Enable or disable the Reset Interval and Start Time fields based on the checkbox state."""
     global reset_enabled
@@ -190,20 +178,22 @@ def enable_reset(reset_checkbox, reset_interval_entry, reset_start_time_dropdown
         reset_interval_entry.config(state='normal')
         reset_start_time_dropdown.config(state='normal')
         reset_enabled = True
-        print("Reset scheduling enabled.")
+        send_to_action_tab("Reset scheduling enabled.")
     else:
         reset_interval_entry.config(state='disabled')
         reset_start_time_dropdown.config(state='disabled')
         reset_enabled = False
-        print("Reset scheduling disabled.")
+        send_to_action_tab("Reset scheduling disabled.")
 
 def schedule_resets():
     """Schedule server resets at the specified interval."""
-    global reset_enabled, reset_interval, reset_start_time, next_reset_time, program_running
+    global reset_enabled, reset_interval, reset_start_time, next_reset_time, program_running, server_running
     while program_running:  # This ensures the loop will stop when the program ends
         if reset_enabled:
+            
             now = datetime.now()
             next_reset = datetime.combine(now.date(), reset_start_time)
+            
             while next_reset <= now:
                 next_reset += timedelta(hours=reset_interval)
 
@@ -220,37 +210,27 @@ def schedule_resets():
             time.sleep(time_to_wait)
 
             send_to_discord("Server is restarting. Please wait until the server is back up to reconnect.")
-            print("Server is restarting")
-            stop_server()
-
-            # Track reset thread as an active process (even though it's not a subprocess)
-            reset_process = threading.Thread(target=start_batch_script, daemon=True)
-            
-            # Add the thread to the active threads list
-            active_processes.append(reset_process)
-            
-            # Manually treat the thread as an active process
-            send_to_process_tab(f"Scheduled reset subprocess started (Thread ID: {reset_process.ident}, Name: {reset_process.name}).")
-
-            reset_process.start()
-
-            # Wait for the thread to complete before removing it
-            reset_process.join()  # This blocks the current thread until reset_process finishes
+            send_to_action_tab("Server is restarting")
+            try:
+                stop_server()
+                send_to_action_tab("Server successfully shut down.  Will begin reboot in 30 seconds.")
+                threading.Timer(30).start()
+                send_to_action_tab("Beginning Server Reboot.")
+                start_batch_script()
+            except Exception as e:
+                send_to_action_tab(f"Error Restarting Server.  Please manually shut down. Error: {e}")
 
             time.sleep(120)  # Wait 2 minutes to ensure the server is stopped before restarting
         else:
             # If reset scheduling is disabled, sleep briefly before checking again
             time.sleep(60)
-            
-            # Remove the thread from the active threads list after it finishes
-            active_processes.remove(reset_process)
 
 def apply_reset_settings(interval_var, start_time_var):
     """Apply the reset settings and log the changes."""
     global reset_interval, reset_start_time, next_reset_time
     reset_interval = interval_var.get()
     reset_start_time = datetime.strptime(start_time_var.get().strip(), "%I:%M %p").time()
-    print(f"Reset settings applied. Interval: {reset_interval} hours, Start Time: {reset_start_time}.")
+    send_to_action_tab(f"Reset settings applied. Interval: {reset_interval} hours, Start Time: {reset_start_time}.")
 
     now = datetime.now()
     next_reset = datetime.combine(now.date(), reset_start_time)
@@ -258,7 +238,7 @@ def apply_reset_settings(interval_var, start_time_var):
         next_reset += timedelta(hours=reset_interval)
 
     next_reset_time = next_reset
-    print(f"Next reset will occur at: {next_reset_time}")
+    send_to_action_tab(f"Next reset will occur at: {next_reset_time}")
 
 def apply_webhook_settings(webhook_url_var):
     """Apply the webhook URL setting."""
@@ -272,7 +252,7 @@ def clear_text_boxes():
     text_area_warning.delete(1.0, tk.END)
     text_area_error.delete(1.0, tk.END)
     text_area_join_code.delete(1.0, tk.END)
-    text_area_console.delete(1.0, tk.END)
+    text_area_action.delete(1.0, tk.END)
     text_area_processes.delete(1.0, tk.END)
     
     
@@ -296,9 +276,10 @@ def on_window_close():
     # Wait for 5 seconds before closing the window
     window.after(3000, close_program)
 
+# %% GUI
 def create_gui():
     """Create the Tkinter GUI."""
-    global window, text_area_main, text_area_all, text_area_warning, text_area_error, text_area_join_code, text_area_console, text_area_processes, program_running
+    global window, text_area_main, text_area_all, text_area_warning, text_area_error, text_area_join_code, text_area_action, text_area_processes, program_running
 
     window = tk.Tk()
     window.title("Tyr VSM")
@@ -313,7 +294,7 @@ def create_gui():
     tab_all = tk.Frame(notebook)
     tab_warning = tk.Frame(notebook)
     tab_error = tk.Frame(notebook)
-    tab_console = tk.Frame(notebook)
+    tab_action = tk.Frame(notebook)
     tab_active_processes = tk.Frame(notebook)
 
     notebook.add(tab_main, text="Session Info")
@@ -323,7 +304,7 @@ def create_gui():
     notebook.add(tab_all, text="All Messages")
     notebook.add(tab_warning, text="Warnings")
     notebook.add(tab_error, text="Errors")
-    notebook.add(tab_console, text="Console Printout")  
+    notebook.add(tab_action, text="Action Updates")  
     notebook.add(tab_active_processes, text="Processes")
 
     text_area_main = scrolledtext.ScrolledText(tab_main, width=80, height=10, wrap=tk.WORD)
@@ -336,8 +317,8 @@ def create_gui():
     text_area_error.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
     text_area_join_code = scrolledtext.ScrolledText(tab_join_code, width=80, height=20, wrap=tk.WORD)
     text_area_join_code.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-    text_area_console = scrolledtext.ScrolledText(tab_console, width=80, height=20, wrap=tk.WORD)
-    text_area_console.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+    text_area_action = scrolledtext.ScrolledText(tab_action, width=80, height=20, wrap=tk.WORD)
+    text_area_action.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
     text_area_processes = scrolledtext.ScrolledText(tab_active_processes, width=80, height=20, wrap=tk.WORD)
     text_area_processes.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
@@ -360,7 +341,7 @@ def create_gui():
     def generate_half_hour_intervals():
         intervals = []
         for hour in range(0, 24):
-            for minute in [0, 30]:
+            for minute in [0, 30]: 
                 # Convert hour to 12-hour format and append AM/PM
                 period = "AM" if hour < 12 else "PM"
                 display_hour = hour % 12 if hour % 12 != 0 else 12
@@ -404,3 +385,4 @@ def create_gui():
 
 if __name__ == "__main__":
     create_gui()
+# %%
